daec19965b5d726a0587e9682c72dd340e22a67d
Oussama El Mouekken <oel-moue@c3r10p4.1337.ma>
2024-12-20 12:26:42 +0100

    with probleme in facing W

--- raycasting.c
+++ raycasting.c
@@ -6,7 +6,7 @@
 /*   By: oel-moue <oel-moue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 17:24:52 by oel-moue          #+#    #+#             */
-/*   Updated: 2024/12/19 23:51:21 by oel-moue         ###   ########.fr       */
+/*   Updated: 2024/12/20 12:22:17 by oel-moue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,26 @@
 double	calcule_first_intersection_with_x(t_data *data)
 {
 	double	first_intersection_by_x;
+	double	player_x;
+	double	player_y;
 
+	player_x = data->player->player_x * SQUAR_SIZE;
+	player_y = data->player->player_y * SQUAR_SIZE;
 	if (data->raycas->is_ray_facing_up)
 	{
-		data->raycas->y_first_point_with_x_intersection = floor(data->player->player_y
-				/ SQUAR_SIZE) * SQUAR_SIZE;
+		data->raycas->y_first_point_with_x_intersection = floor(player_y
+				/ SQUAR_SIZE) * SQUAR_SIZE - 0.0001;
 		data->raycas->y_step = -SQUAR_SIZE;
 	}
 	else
 	{
-		data->raycas->y_first_point_with_x_intersection = floor(data->player->player_y
+		data->raycas->y_first_point_with_x_intersection = floor(player_y
 				/ SQUAR_SIZE) * SQUAR_SIZE + SQUAR_SIZE;
 		data->raycas->y_step = SQUAR_SIZE;
 	}
-	first_intersection_by_x = data->player->player_x
-		+ (data->raycas->y_first_point_with_x_intersection
-			- data->player->player_y) / tan(data->raycas->ray_angle);
+	first_intersection_by_x = player_x
+		+ (data->raycas->y_first_point_with_x_intersection - player_y)
+		/ tan(data->raycas->ray_angle);
 	data->raycas->x_step = data->raycas->y_step / tan(data->raycas->ray_angle);
 	if ((data->raycas->is_ray_facing_left && data->raycas->x_step > 0)
 		|| (data->raycas->is_ray_facing_right && data->raycas->x_step < 0))
@@ -43,22 +47,26 @@ double	calcule_first_intersection_with_x(t_data *data)
 double	calcule_first_intersection_with_y(t_data *data)
 {
 	double	first_intersection_by_y;
+	double	player_x;
+	double	player_y;
 
+	player_x = data->player->player_x * SQUAR_SIZE;
+	player_y = data->player->player_y * SQUAR_SIZE;
 	if (data->raycas->is_ray_facing_left)
 	{
-		data->raycas->x_first_point_with_y_intersection = floor(data->player->player_x
-				/ SQUAR_SIZE) * SQUAR_SIZE;
+		data->raycas->x_first_point_with_y_intersection = floor(player_x
+				/ SQUAR_SIZE) * SQUAR_SIZE + 0.0001;
 		data->raycas->x_step = -SQUAR_SIZE;
 	}
 	else
 	{
-		data->raycas->x_first_point_with_y_intersection = floor(data->player->player_x
+		data->raycas->x_first_point_with_y_intersection = floor(player_x
 				/ SQUAR_SIZE) * SQUAR_SIZE + SQUAR_SIZE;
 		data->raycas->x_step = SQUAR_SIZE;
 	}
-	first_intersection_by_y = data->player->player_y
-		+ (data->raycas->x_first_point_with_y_intersection
-			- data->player->player_x) * tan(data->raycas->ray_angle);
+	first_intersection_by_y = player_y
+		+ (data->raycas->x_first_point_with_y_intersection - player_x)
+		* tan(data->raycas->ray_angle);
 	data->raycas->y_step = data->raycas->x_step * tan(data->raycas->ray_angle);
 	if ((data->raycas->is_ray_facing_up && data->raycas->y_step > 0)
 		|| (data->raycas->is_ray_facing_down && data->raycas->y_step < 0))
@@ -70,21 +78,27 @@ double	calcule_first_intersection_with_y(t_data *data)
 
 int	is_wall(double x, double y, t_data *data)
 {
-	int	i;
-	int	j;
+	int	map_x;
+	int	map_y;
 
-	i = floor(x / SQUAR_SIZE);
-	j = floor(y / SQUAR_SIZE);
-	if (i < 0 || i >= data->w || j < 0 || j >= data->h)
+	// We're already working with pixel coordinates, so adjust the division
+	map_x = floor(x) / SQUAR_SIZE;
+	map_y = floor(y) / SQUAR_SIZE;
+	if (map_x < 0 || map_x >= data->w || map_y < 0 || map_y >= data->h)
 		return (1);
-	if (data->map[j][i] == '1')
+	if (data->map[map_y][map_x] == '1')
 		return (1);
 	return (0);
 }
 
 double	distance_horizontal(t_data *data)
 {
+	double	player_x;
+	double	player_y;
+
 	double x, y;
+	player_x = data->player->player_x * SQUAR_SIZE;
+	player_y = data->player->player_y * SQUAR_SIZE;
 	x = calcule_first_intersection_with_x(data);
 	y = data->raycas->y_first_point_with_x_intersection;
 	while (x >= 0 && x < data->img->width && y >= 0 && y < data->img->height)
@@ -96,33 +110,32 @@ double	distance_horizontal(t_data *data)
 	}
 	data->raycas->end_x_horizontal = x;
 	data->raycas->end_y_horizontal = y;
-	data->raycas->distance_horizontal = sqrt(pow(x - (data->player->player_x
-					* SQUAR_SIZE), 2) + pow(y - (data->player->player_y
-					* SQUAR_SIZE), 2));
+	data->raycas->distance_horizontal = sqrt(pow(x - player_x, 2) + pow(y
+				- player_y, 2));
 	return (data->raycas->distance_horizontal);
 }
 
 double	distance_vertical(t_data *data)
 {
+	double	player_x;
+	double	player_y;
+
 	double x, y;
+	player_x = data->player->player_x * SQUAR_SIZE;
+	player_y = data->player->player_y * SQUAR_SIZE;
 	y = calcule_first_intersection_with_y(data);
 	x = data->raycas->x_first_point_with_y_intersection;
 	while (x >= 0 && x < data->img->width && y >= 0 && y < data->img->height)
 	{
-		// printf("1\n");
-		// 	exit(0); when i have a probleme 
 		if (is_wall(x, y, data))
-		{
 			break ;
-		}
 		x += data->raycas->x_step;
 		y += data->raycas->y_step;
 	}
 	data->raycas->end_x_vertical = x;
 	data->raycas->end_y_vertical = y;
-	data->raycas->distance_vertical = sqrt(pow(x - (data->player->player_x
-					* SQUAR_SIZE), 2) + pow(y - (data->player->player_y
-					* SQUAR_SIZE), 2));
+	data->raycas->distance_vertical = sqrt(pow(x - player_x, 2) + pow(y
+				- player_y, 2));
 	return (data->raycas->distance_vertical);
 }
 
@@ -143,12 +156,12 @@ void	draw_ray(t_data *data, double distance, double ray_angle)
 	double	x0;
 	double	y0;
 
+	(void)distance;
+	(void)ray_angle;
 	x0 = data->player->player_x * SQUAR_SIZE;
 	y0 = data->player->player_y * SQUAR_SIZE;
-	x_end = data->player->player_x * SQUAR_SIZE + distance * cos(ray_angle);
-	y_end = data->player->player_y * SQUAR_SIZE + distance * sin(ray_angle);
-	printf("ray_angle: %f, distance: %f\n", ray_angle, distance);
-	printf("x0: %f, y0: %f, x_end: %f, y_end: %f\n", x0, y0, x_end, y_end);
+	x_end = data->raycas->wall_hit_x;
+	y_end = data->raycas->wall_hit_y;
 	ft_draw_line(data, x0, y0, x_end, y_end);
 }
 
